335b6fdf2413a931c92d3ffd6fb4585d0f0a8ac1
Eagle-lucid <vicokefie@gmail.com>
2024-11-15 04:36:12 -0500

    Problem: I ran into an issue where by i want the user to click outside the 'modal-description' to clear the description.

--- index.js
+++ index.js
@@ -5,36 +5,42 @@ const bookData = {
         author: 'George Orwell',
         description: 'A dystopian novel set in a totalitarian society ruled by Big Brother.',
         pages: 328,
+        image: 'images/1984.jpg',
     },
     'Becoming': {
         title: 'Becoming',
         author: 'Michelle Obama',
         description: 'A memoir by the former First Lady of the United States, Michelle Obama.',
         pages: 426,
+        image: 'images/becoming.jpg',
     },
     'Educated': {
         title: 'Educated',
         author: 'Tara Westover',
         description: 'A memoir about a woman who grows up in a strict and abusive household in rural Idaho and later escapes to learn about the world through education.',
         pages: 334,
+        image: 'images/educated.jpg',
     },
     'To-Kill-a-Mockingbird': {
         title: 'To Kill a Mockingbird',
         author: 'Harper Lee',
         description: 'A novel about racial injustice in the Deep South, as seen through the eyes of young Scout Finch.',
         pages: 281,
+        image: 'images/mocking.jpg',
     },
     'Sapiens': {
         title: 'Sapiens: A Brief History of Humankind',
         author: 'Yuval Noah Harari',
         description: "A historical account of humankind's evolution, exploring biology, anthropology, and economics.",
         pages: 498,
+        image: 'images/sapiens.jpg',
     },
     'TheGreatGatsby': {
         title: 'The Great Gatsby',
         author: 'F. Scott Fitzgerald',
         description: 'A novel about the American dream and the disillusionment of post-war America, as experienced by Jay Gatsby.',
         pages: 180,
+        image: 'images/gatsby.png',
     },
 
 };
@@ -43,16 +49,51 @@ const bookData = {
 function showDetails(bookTitle) {
     const book = bookData[bookTitle];
     const bookDescription = document.getElementById('bookDescription');
+    const mainSection = document.querySelector('main');
+    const addBookButton = document.getElementById('addBookButton');
 
     if(book) {
         bookDescription.innerHTML =
-        `<h3>${book.title} by ${book.author}</h3>
-        <p><strong>Description:</strong>${book.description}</p>
-        <p><strong>Pages:</strong>${book.pages}</p>
+        `
+            <img src="${book.images}" alt="Cover of ${book.title}">
+            <h3>Title: ${book.title}</h3>
+            <h3>Author: by ${book.author}</h3>
+            <p><strong>Description: </strong>${book.description}</p>
+            <p><strong>Pages: </strong>${book.pages}</p>
+            <div class="status">
+                <label class="status-txt">Status:</label>
+                
+                <label for="read">
+                    <input type="radio" name="status" id="read" value="read" required>
+                    Read
+                </label>
+                
+                <label for="unread">
+                    <input type="radio" name="status" id="unread" value="unread" required>
+                    Unread
+                </label>
+            </div>
+            <button type="submit" id="addBookButton">Add Book</button>
         `;
-        bookDescription.style.display = 'block';
+        bookDescription.style.visibility = 'visible';
+        mainSection.classList.add('main-blurred');
+        addBookButton.classList.add('hidden');
     }
 }
+
+// Function to cancel the modal when user click outside the modal
+function outsideClickListener(event) {
+    const bookDescription = document.getElementById('bookDescription');
+    const mainSection = document.querySelector('main');
+    const addBookButton = Document.getElementById('addBookButton');
+
+    outsideClickListener.addEventListener('click', () => {
+        bookDescription.classList.add('visible');
+        mainSection.classList.remove('main-blurred');
+        addBookButton.classList.remove('hidden');
+    })
+}
+
 // Function for hidden form
 const addBookButton = document.getElementById('addBookButton');
 const hiddenForm = document.getElementById('hiddenForm');
